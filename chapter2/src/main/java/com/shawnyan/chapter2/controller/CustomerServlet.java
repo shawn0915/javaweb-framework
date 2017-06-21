8e117016ec77808b8292cc50af69430a32e22899
Shawn Yan <shawn2016@aliyun.com>
2017-06-21 22:19:43 +0800

    chapter 2. CustomerServlet done.

--- chapter2/src/main/java/com/shawnyan/chapter2/controller/CustomerServlet.java
+++ chapter2/src/main/java/com/shawnyan/chapter2/controller/CustomerServlet.java
@@ -1,7 +1,34 @@
 package com.shawnyan.chapter2.controller;
 
+import com.shawnyan.chapter2.model.Customer;
+import com.shawnyan.chapter2.service.CustomerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
 /**
+ * 进入 客户列表 界面
  * Created by SHAWN on 6/21/2017.
  */
-public class CustomerServlet {
+@WebServlet("/customer")
+public class CustomerServlet extends HttpServlet{
+
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException{
+        customerService = new CustomerService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList",customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req,resp);
+    }
 }
--- chapter2/src/main/webapp/WEB-INF/view/customer.jsp
+++ chapter2/src/main/webapp/WEB-INF/view/customer.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: SHAWN
@@ -8,9 +9,31 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>客户管理</title>
 </head>
 <body>
+
+<h1>客户列表</h1>
+
+<table border="1">
+    <tr>
+        <th>客户名称</th>
+        <th>联系人</th>
+        <th>电话号码</th>
+        <th>邮箱地址</th>
+        <th>操作</th>
+    </tr>
+    <c:forEach var="customer" items="${customerList}">
+        <tr>
+            <td>${customer.name}</td>
+            <td>${customer.contact}</td>
+            <td>${customer.telephone}</td>
+            <td>${customer.email}</td>
+            <td><a href="${BASE}/customer_edit?id=${customer.id}">编辑</a></td>
+            <td><a href="${BASE}/customer_delete?id=${customer.id}">删除</a></td>
+        </tr>
+    </c:forEach>
+</table>
 
 </body>
 </html>
